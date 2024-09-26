99a3d2b38607264302377ae34ec7c0fda2b3884c
Ischi94 <gmathes94@gmx.de>
2024-09-26 11:00:59 +0200

    create tables for additional div metrics

--- R/spline_model.R
+++ R/spline_model.R
@@ -1,10 +1,11 @@
 library(here)
 library(tidyverse)
-
+library(writexl)
 
 
 # read data ---------------------------------------------------------------
 
+# pyrate species
 dat_pyrate_species <- read_delim(here("data", 
                                       "all_species_input_PyRate_mcmcdiv.log")) %>% 
   select(it, contains("t_")) %>% 
@@ -18,74 +19,134 @@ dat_pyrate_species <- read_delim(here("data",
   mutate(run = run/500) %>% 
   add_column(metric = "PyRate")
 
+# pyrate genus
+dat_pyrate_genus <- read_delim(here("data", 
+                                      "all_genus_PyRate_mcmcdiv.log")) %>% 
+  select(it, contains("t_")) %>% 
+  pivot_longer(-it, 
+               names_to = "start_age", 
+               values_to = "diversity") %>% 
+  mutate(start_age = str_remove_all(start_age, "t_"), 
+         start_age = as.double(start_age)) %>% 
+  rename(run = it) %>% 
+  filter(run != 0) %>% 
+  mutate(run = run/500) %>% 
+  add_column(metric = "PyRate")
+
+# sqs species
 dat_sqs <- read_rds(here("data",
                          "diversity_continuous_sqs.rds")) %>% 
   select(start_age, diversity = speciesRT) 
 
+# sqs genus
+dat_sqs_genus <- read_rds(here("data",
+                         "diversity_continuous_sqs.rds")) %>% 
+  select(start_age, diversity = speciesRT) 
+
+# raw species
+dat_raw <- read_rds(here("data",
+                         "diversity_continuous_raw.rds")) %>% 
+  select(start_age, diversity = speciesRT) 
+
+# sqs genus
+dat_raw_genus <- read_rds(here("data",
+                               "diversity_continuous_raw.rds")) %>% 
+  select(start_age, diversity = speciesRT) 
+
+# set up spline function --------------------------------------------------
+
+div_per_stage <- function(data_set) {
+  
+  # and predict 
+  dat_mean <- data_set %>%
+    spline(x = .$start_age,
+           y = .$diversity, 
+           ties = "mean",
+           xout = unique(dat_sqs$start_age),
+           method = "natural") %>% 
+    pluck("y") %>% 
+    as_tibble_col(column_name = "mean_div") %>% 
+    add_column(start_age = unique(dat_sqs$start_age),
+               .before = 1)
+  
+  # and predict 
+  dat_min <- data_set %>%
+    spline(x = .$start_age,
+           y = .$diversity, 
+           ties = "min",
+           xout = unique(dat_sqs$start_age),
+           method = "natural") %>% 
+    pluck("y") %>% 
+    as_tibble_col(column_name = "min_div") %>% 
+    add_column(start_age = unique(dat_sqs$start_age),
+               .before = 1)
+  
+  # and predict 
+  dat_max <- data_set %>%
+    spline(x = .$start_age,
+           y = .$diversity, 
+           ties = "max",
+           xout = unique(dat_sqs$start_age),
+           method = "natural") %>% 
+    pluck("y") %>% 
+    as_tibble_col(column_name = "max_div") %>% 
+    add_column(start_age = unique(dat_sqs$start_age),
+               .before = 1)
+  
+  # combine
+  dat_full <- dat_mean %>% 
+    full_join(dat_min) %>% 
+    full_join(dat_max) %>%
+    mutate(min_div = if_else(min_div < 0, 0, min_div)) 
+  
+  return(dat_full)
+  
+}
+
+
 # fit a spline model ------------------------------------------------------
 
-# and predict 
-dat_mean <- dat_pyrate_species %>%
-  spline(x = .$start_age,
-         y = .$diversity, 
-         ties = "mean",
-         xout = unique(dat_sqs$start_age),
-         method = "natural") %>% 
-  pluck("y") %>% 
-  as_tibble_col(column_name = "mean_div") %>% 
-  add_column(start_age = unique(dat_sqs$start_age),
-              .before = 1)
-
-# and predict 
-dat_min <- dat_pyrate_species %>%
-  spline(x = .$start_age,
-         y = .$diversity, 
-         ties = "min",
-         xout = unique(dat_sqs$start_age),
-         method = "natural") %>% 
-  pluck("y") %>% 
-  as_tibble_col(column_name = "min_div") %>% 
-  add_column(start_age = unique(dat_sqs$start_age),
-             .before = 1)
-
-# and predict 
-dat_max <- dat_pyrate_species %>%
-  spline(x = .$start_age,
-         y = .$diversity, 
-         ties = "max",
-         xout = unique(dat_sqs$start_age),
-         method = "natural") %>% 
-  pluck("y") %>% 
-  as_tibble_col(column_name = "max_div") %>% 
-  add_column(start_age = unique(dat_sqs$start_age),
-             .before = 1)
-  
-# combine
-dat_full <- dat_mean %>% 
-  full_join(dat_min) %>% 
-  full_join(dat_max) %>%
-  mutate(min_div = if_else(min_div < 0, 0, min_div)) 
-  
-
-# visualise
-dat_full %>% 
-  ggplot(aes(start_age)) +
-  geom_point(aes(y = diversity), 
-             data = dat_pyrate_species) +
-  geom_point(aes(y = min_div), 
-             colour = "coral") +
-  geom_point(aes(y = mean_div), 
-            colour = "steelblue") +
-  geom_point(aes(y = max_div), 
-             colour = "firebrick") 
-  
-# save 
-dat_full %>% 
-  write_rds(here("data", 
-               "pyrate_species_binned.rds"))
+# species
+div_per_stage(dat_pyrate_species) %>% 
+  # save 
+  write_xlsx(here("data", "taxa_per_stage_species_pyrate.xlsx"))
 
 
-# set up function ---------------------------------------------------------
+# genus
+div_per_stage(dat_pyrate_genus) %>% 
+  # save 
+  write_xlsx(here("data", "taxa_per_stage_genus_pyrate.xlsx"))
+
+
+# set up summary function -------------------------------------------------
+
+sum_per_stage <- function(data_set) {
+  data_set %>% 
+  group_by(start_age) %>% 
+  summarise(mean_dif = mean(diversity), 
+            min_dif = min(diversity), 
+            max_dif = max(diversity))
+  }
+
+# raw species
+sum_per_stage(dat_raw) %>% 
+  write_xlsx(here("data", "taxa_per_stage_species_raw.xlsx"))
+
+# raw genus
+sum_per_stage(dat_raw_genus)%>% 
+  write_xlsx(here("data", "taxa_per_stage_genus_raw.xlsx"))
+
+# sqs species
+sum_per_stage(dat_sqs)%>% 
+  write_xlsx(here("data", "taxa_per_stage_raw_sqs.xlsx"))
+
+# sqs genus
+sum_per_stage(dat_sqs_genus)%>% 
+  write_xlsx(here("data", "taxa_per_stage_genus_sqs.xlsx"))
+
+
+
+# set up perc change function ---------------------------------------------------------
 
 get_perc_change <- function(data_set, start_age_vec) {
   
