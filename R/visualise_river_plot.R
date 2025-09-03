ddb6e6798d19c1963cac287008c2580305459c3d
Mathes <mathes06@ads.uni-passau.de>
2025-09-03 09:37:39 +0200

    add alluvial plot

--- R/visualise_river_plot.R
+++ R/visualise_river_plot.R
@@ -1,130 +1,105 @@
-library(tidyverse)
+# load libraries
 library(here)
-library(patchwork)
-library(deeptime)
-library(pammtools)
+library(readxl)
+library(tidyverse)
+library(ggalluvial)
 
 
-# read data ---------------------------------------------------------------
 
 
-dat_deep_species <- read_csv(here("data",
-                                  "deepdive_species",
-                                  "all_64.csv")) %>% 
-  bind_rows(read_csv(here("data",
-                          "deepdive_species",
-                          "all_128.csv"))) %>% 
-  rowid_to_column("run") %>% 
-  pivot_longer(cols = -run, 
-               names_to = "start_age", 
-               values_to = "DeepDive") %>% 
-  mutate(start_age = as.double(start_age))
+dat_ocean <- read_xlsx(here("data", "Paleo_ocean_timeline.xlsx"), 
+                       sheet = 'Time_Series')
 
-# stage data
-epoch_age <- read_rds(here("data", 
-                           "epoch_age.rds"))
+dat_ocean_age <- read_xlsx(here("data", "Paleo_ocean_timeline.xlsx"),
+                           sheet = 1) %>% 
+  select(basin = `Paleo-Ocean Basin`, 
+         epoch = `Start Epoch`, 
+         age = `Start MA`)
 
 epoch_cor <- read_rds(here("data",
                            "epoch_cor.rds"))
 
-stage_cor <- read_rds(here("data", 
-                           "stage_cor.rds"))
-# visualise ------------------------------------------------------------------
+dat_fins <- read_xlsx(here("data",
+                           "species.xlsx"))
 
-# reframe
-dat_plot <-  dat_deep_species %>%
-  group_by(start_age) %>% 
-  summarise(ymin = min(DeepDive, na.rm = TRUE), 
-            y = mean(DeepDive, na.rm = TRUE), 
-            ymax = max(DeepDive, na.rm = TRUE)) %>%   
-  mutate(start_age = as.double(start_age))
 
-# line plot
-plot_line <- dat_plot %>% 
-  ggplot(aes(start_age, y)) +
-  geom_vline(xintercept = epoch_age,
-             colour = "grey90") +
-  geom_ribbon(aes(ymin = ymin, 
-                  ymax = ymax), 
-              alpha = 0.3, 
-              fill = "grey20", 
-              colour = "white") +
-  geom_point(shape = 21, 
-             colour = "grey20", 
-             fill = "white") +
-  geom_line(colour = "grey20") +
-  labs(y = "Species Diversity",
-       x = "Myr") +
-  scale_x_reverse(breaks = seq(140, 0, by = -20), 
-                  limits = c(146, -5)) +
-  scale_y_continuous(breaks = seq(0, 2500, by = 500), 
-                     limits = c(0, 2800)) +
-  coord_geo(dat = list(stage_cor, 
-                       epoch_cor, 
-                       "periods"),
-            pos = list("b", "b", "b"),
-            alpha = 0.2,
-            height = list(unit(1.25, "line"), 
-                          unit(0.75, "line"), 
-                          unit(0.75, "line")),
-            size = list(6/.pt, 6/.pt, 6/.pt),
-            lab_color = "grey20",
-            color = "grey20",
-            abbrv = list(TRUE, TRUE, FALSE),
-            rot = list(90, 0, 0),
-            # fill = "white",
-            expand = FALSE,
-            lwd = list(0.1, 0.1, 0.1)) +
+
+dat_ocean_clean <- dat_ocean %>%
+  select(-Count) %>% 
+  pivot_longer(cols = everything(), 
+               values_to = "basin", 
+               names_to = "epoch") %>% 
+  mutate(epoch = str_replace_all(epoch, "_", " ")) %>% 
+  left_join(dat_fins %>% 
+              group_by(paleoocean, epoch = early_epoch) %>% 
+              summarise(pal_lat = mean(latitude)) %>% 
+              ungroup() %>% 
+              select(basin = paleoocean, pal_lat, epoch) %>% 
+              distinct(epoch, basin, pal_lat)) %>% 
+  left_join(dat_fins %>% 
+              distinct(accepted_name, paleoocean, early_epoch) %>%
+              count(basin = paleoocean, epoch = early_epoch)) %>% 
+  left_join(epoch_cor %>% 
+              select(epoch = name, start_age = max_age) %>% 
+              mutate(epoch = str_replace_all(epoch, "Late", "Upper"), 
+                     epoch = str_replace_all(epoch, "Early", "Lower"), 
+                     epoch = str_replace_all(epoch, "Pli", "Pliocene"),
+                     epoch = str_replace_all(epoch, "Ple", "Pleistocene"))) %>% 
+  add_column(id = rep(1:11, each = 9)) %>%
+  left_join(tibble(basin = unique(.$basin), 
+                   basin_leg = c("Pacific", "WIS", 
+                                 "Atlantic", "WT", "Tethys", 
+                                 "Neo-Tethys", "TS", "SAES", 
+                                 "Arctic", "TSS", 
+                                 "Mediterranean", "Southern", "Indian"))) %>% 
+  filter(epoch != "Holocene") %>% 
+  mutate(epoch = str_replace_all(epoch, " ", "\n"), 
+         epoch = fct_reorder(epoch, start_age),
+         basin_leg = fct_reorder(basin_leg, desc(pal_lat), 
+                                 .na_rm = TRUE)) %>% 
+  replace_na(list(n = 1)) 
+
+
+plot_ocean <- dat_ocean_clean %>%
+  ggplot(aes(x = epoch, stratum = basin_leg, log(1+n),
+             alluvium = id, fill = basin_leg, label = basin_leg)) +
+  geom_flow(aes(colour = basin_leg), 
+            stat = "alluvium", lode.guidance = "frontback", 
+            alpha = 1, 
+            width = 0) +
+  geom_label(aes(colour = basin_leg),
+             stat = "stratum",  
+             label.r = unit(0.5, "lines"),
+             label.size = 0, 
+             size = 6/.pt,
+             fill = "white", 
+             show.legend = FALSE) +
+  scale_size_identity(guide = "legend") +
+  scale_fill_viridis_d(labels = dat_ocean_clean %>%
+                         mutate(basin = fct_reorder(basin,
+                                                    desc(pal_lat),
+                                                    .na_rm = TRUE)) %>%
+                         distinct(basin) %>%
+                         arrange(basin) %>%
+                         pull(basin)) +
+  scale_colour_viridis_d(guide = 'none') +
+  scale_x_discrete(limits = rev) +
   theme_minimal() +
-  theme(legend.position = "none",
-        axis.ticks = element_line(colour = "grey50"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-
-
-# step plot
-plot_step <- dat_plot %>%
-  ggplot(aes(start_age, y)) +
-  geom_vline(xintercept = epoch_age,
-             colour = "grey90") +
-  geom_stepribbon(aes(ymin = ymin, 
-                      ymax = ymax), 
-                  alpha = 0.15, 
-                  fill = "grey20",
-                  colour = "white", 
-                  linewidth = 0.001) +
-  geom_step(colour = "grey20") +
-  labs(y = "Genus Diversity",
-       x = "Myr",
-       colour = NULL) +
-  labs(y = "Species Diversity",
-       x = "Myr") +
-  scale_x_reverse(breaks = seq(140, 0, by = -20), 
-                  limits = c(146, -5)) +
-  scale_y_continuous(breaks = seq(0, 2500, by = 500), 
-                     limits = c(0, 2800)) +
-  coord_geo(dat = "epochs",
-            pos = "b",
-            alpha = 0,
-            lab_color = "white",
-            color = "white",
-            abbrv = TRUE,
-            fill = "white",
-            expand = FALSE) +
-  theme_minimal() +
-  theme(legend.position = "none",
-        axis.ticks = element_line(colour = "grey50"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank())
-
-# patch together
-# plot_fin <- 
-plot_step / plot_line
+  guides(fill = guide_legend(ncol = 4, byrow = TRUE)) +
+  labs(x = NULL, y = "Occurrences",
+       fill = NULL, colour = NULL) +
+  theme(axis.text.x = element_text(angle = 20, hjust = 0.5), 
+        axis.text.y = element_blank(),
+        panel.grid = element_blank(),
+        # legend.position = c(0.85, 0.9),
+        legend.position = "bottom",
+        legend.key.size = unit(3, "mm"), 
+        legend.text = element_text(size = 7))
 
 # save
-ggsave(plot_fin, 
+ggsave(plot_ocean, 
        filename = here("figures",
                        "fig_S1.pdf"), 
-       width = 183, height = 100*1.5,
+       width = 183*1.5, height = 150,
        units = "mm", 
        bg = "white")
